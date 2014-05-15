e0ab008a13e592e6760c6671efc02699e92fd14a
eric <ericzsf@guanxi.me>
2014-05-15 17:59:00 +0800

    test commit

--- EricIosDemo/EricIosDemo/Constans/DescContent.h
+++ EricIosDemo/EricIosDemo/Constans/DescContent.h
@@ -9,15 +9,20 @@
 #ifndef EricIosDemo_DescContent_h
 #define EricIosDemo_DescContent_h
 
-//project releated
+//************
+
+//************project releated
 
 /*
 非ARC的文件 -fno-objc-arc
+ 
+eric iphone token b2c2b5002ba5e1c19b79804116b000df721281e7a43575999f20834e5d9c492f
+
 */
 
 
 
-//Git releated
+//************Git releated
 /*
  $ git tag -a v1.4 -m 'my version 1.4'
  $ git push origin v1.4
