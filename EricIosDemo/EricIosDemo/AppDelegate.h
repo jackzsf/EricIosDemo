01f07f2ff8e31fd9054f1c658891319126a0bec4
eric <ericzsf@guanxi.me>
2014-05-15 16:50:27 +0800

    add EricIosDemo project

---
+++ EricIosDemo/EricIosDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  EricIosDemo
+//
+//  Created by Eric on 14-5-15.
+//  Copyright (c) 2014年 Eric. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ EricIosDemo/EricIosDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  EricIosDemo
+//
+//  Created by Eric on 14-5-15.
+//  Copyright (c) 2014年 Eric. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
